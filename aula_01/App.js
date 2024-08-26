d55735226e2a37760ac063ab7a8900bab35032ee
josefabioguimaraes <jfabioguimaraes@gmail.com>
2024-08-26 17:13:20 -0300

    Add styles on app.js

--- aula_01/App.js
+++ aula_01/App.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import Exercicio1 from './src/components/Exercicio1'
 import Exercicio2 from './src/components/Exercicio2'
@@ -7,10 +7,18 @@ import Exercicio4 from './src/components/Exercicio4'
 
 const App = () => {
   return (
-    <View>
-      <Exercicio4/>
+    <View style={styles.container}>
+      <Exercicio4 />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
 
 export default App
